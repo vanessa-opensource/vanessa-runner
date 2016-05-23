d7e4c65c2b903027a629e1f8beec4a4452f3047e
Сосна Евгений <shenja@sosna.zp.ua>
2016-05-23 21:07:15 +0300

    Добавил чтение параметров из переменных среды.

--- tools/compile_epf.bat
+++ tools/compile_epf.bat
@@ -5,6 +5,7 @@ SET USERPWD=
 rem SET connstring=--ibname /F"C:\Users\eugens\Documents\Rarus\ITIL\1"
 rem SET USERPWD=--db-user base --db-pwd 234567890
 SET RUNNER_ENV=production
+rem SET RUNNER_V8VERSION=8.3.8.1652
 
-oscript %mypath%/runner.os compileepf .\epf\ .\build\epf\ %connstring% %USERPWD% 
+oscript %mypath%/runner.os compileepf .\epf\ .\build\epf\ %connstring% %USERPWD%
 
